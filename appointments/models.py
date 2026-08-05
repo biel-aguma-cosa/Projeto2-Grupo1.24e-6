79fa5ffff3e9168ebf383ff704c7bf8795dde16d
biel-aguma-cosa <49573010+biel-aguma-cosa@users.noreply.github.com>
2026-08-05 20:43:58 -0300

    aaaaa

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Medic(models.Model):
+    name = models.TextField(max_length=50,verbose_name='Nome')
+    last_name = models.TextField(max_length=40,verbose_name='Sobrenome')
+    
+class Patient(models.Model):
+    name      = models.TextField(max_length=20,verbose_name='Nome')
+    last_name = models.TextField(max_length=40,verbose_name='Sobrenome')
+
+class Appointment(models.Model):
+    medic   = models.ForeignKey(Medic  , models.PROTECT)
+    patient = models.ForeignKey(Patient, models.PROTECT)
+    date    = models.DateField(verbose_name='Data')
