7ff637a6de6ff40e79a9883f3ae1b00fbed38ea0
biel-aguma-cosa <49573010+biel-aguma-cosa@users.noreply.github.com>
2026-08-05 21:53:56 -0300

    models + admin (em progresso)

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import  Appointment, MedicQualification, Patient, Medic, Qualification
 
 # Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display  = ('patient','medic','date','time','subject','details','created_at','updated_at')
+    search_fields = ('name'   ,'date'        )
+    list_filter   = ('name'   ,'date' ,'time')
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display  = ('patient','medic','date','time','subject','details','created_at','updated_at')
+    search_fields = ('name'   ,'date'        )
+    list_filter   = ('name'   ,'date' ,'time')
+ 
--- appointments/models.py
+++ appointments/models.py
@@ -1,15 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Qualification(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Título')
+    class Meta:
+        ordering     = ['name']
+        verbose_name = 'Qualificação'
+
 class Medic(models.Model):
-    name = models.TextField(max_length=50,verbose_name='Nome')
-    last_name = models.TextField(max_length=40,verbose_name='Sobrenome')
+    name           = models.CharField(max_length=50,verbose_name='Nome')
+    last_name      = models.TextField(max_length=40,verbose_name='Sobrenome')
+    qualifications = models.ManyToManyField(
+        Qualification, through='MedicQualification', related_name='medics')
+    class Meta:
+        ordering     = ['name']
+        verbose_name = 'Paciente'
+
+class MedicQualification(models.Model):
+    medic         = models.ForeignKey(Medic        , on_delete=models.CASCADE)
+    qualification = models.ForeignKey(Qualification, on_delete=models.CASCADE)
+
+    aquired_at = models.DateField(verbose_name='Adquirida em')
+
     
 class Patient(models.Model):
     name      = models.TextField(max_length=20,verbose_name='Nome')
     last_name = models.TextField(max_length=40,verbose_name='Sobrenome')
+    class Meta:
+        ordering     = ['name']
+        verbose_name = 'Paciente'
 
 class Appointment(models.Model):
-    medic   = models.ForeignKey(Medic  , models.PROTECT)
-    patient = models.ForeignKey(Patient, models.PROTECT)
-    date    = models.DateField(verbose_name='Data')
+    medic   = models.ForeignKey(Medic  , models.PROTECT, related_name='appointments', verbose_name='Profissional')
+    patient = models.ForeignKey(Patient, models.PROTECT, related_name='appointments', verbose_name='Paciente'    )
+
+    subject = models.CharField(verbose_name='Assunto'  , max_length=50            )
+    date    = models.DateField(verbose_name='Data'                                )
+    time    = models.TimeField(verbose_name='Hora'                                )
+    details = models.TextField(verbose_name='Detalhes' , null = True, blank = True)
+
+    created_at = models.DateTimeField(auto_add_now=True, verbose_name='Criada em'    )
+    updated_at = models.DateTimeField(auto_now    =True, verbose_name='Atualizada em')
+    class Meta:
+        ordering = ['date', 'time']
+        verbose_name = 'Consulta'
