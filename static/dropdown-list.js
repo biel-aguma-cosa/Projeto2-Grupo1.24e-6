21ce00d04b62fff27d6c7daaa82632cd296c6a20
biel-aguma-cosa <49573010+biel-aguma-cosa@users.noreply.github.com>
2026-08-10 21:47:30 -0300

    consertar dropdown-list.js

--- static/dropdown-list.js
+++ static/dropdown-list.js
@@ -6,15 +6,32 @@ function main() {
     for (const DIV of DIVS) {
         const select  = DIV.getElementsByTagName('select' )[0]
         const display = DIV.getElementsByTagName('section')[0]
-        select.addEventListener('change', (event) => {
-            block = document.createElement('input')
 
-            block.readOnly  = True
-            block.type      = 'text'
-            block.value     = event.target.value
-            block.className = 'dropdown-list-block'
+        DIV.array   = []
+        select.opened = false
+        select.div    = DIV
 
-            display.appendChild(block)
+        select.addEventListener('click', (event) => {
+            event.target.opened = !event.target.opened
+            const VALUE = event.target.value
+            if ((!event.target.opened) && (!event.target.div.array.includes(VALUE))) {
+                block = document.createElement('input')
+
+                block.readOnly  = true
+                block.type      = 'text'
+                block.name      = 'qualification'
+                block.value     = VALUE
+                block.className = 'dropdown-list-block'
+
+                DIV.array.push(VALUE)
+
+                display.appendChild(block)
+
+                console.log(block)
+                console.log(DIV.array)
+
+                event.target.value = 'add qualification'
+            }
         })
     }
 }
